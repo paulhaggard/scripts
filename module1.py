52682fee34cf7f48385897d48d5992ab80491a4a
Paul Haggard <paul.haggard@jetinc.net>
2018-12-06 13:46:27 -0600

    .

--- module1.py
+++ module1.py
@@ -2,11 +2,12 @@ import sys, configparser
 
 config = configparser.ConfigParser()
 try:
-    dataset = config.read('/home/displayboard/ftp/files/Host_0/cfg1.ini')
+    dataset = config.read('/home/displayboard/ftp/files/Host_0/cfg.ini')
 except:
     print("Unexpected error:", sys.exc_info()[0])
 if len(dataset) <= 0:
     print("missing file")
+    return_val = 1
 else:
     if 'bitbucket.org' in config:
         print("bitbucket found")
@@ -15,3 +16,4 @@ else:
     slideshow = config['slideshow']
     slideshow['Set_switch']
     print(slideshow['Slide_time'])
+    return_val = 0
