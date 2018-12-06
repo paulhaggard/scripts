3a66a86e392b474744ca243f4b1d9bfcdcde1bf0
Paul Haggard <paul.haggard@jetinc.net>
2018-12-06 13:59:07 -0600

    .

--- module1.py
+++ module1.py
@@ -16,7 +16,8 @@ else:
     slideshow = config['slideshow']
     slideshow['Set_switch']
     print(slideshow['Slide_time'])
-    subprocess_cmd("mkdir /home/pi/scripts/configs && sudo cp /etc/dhcpcd.conf.ap /home/pi/scripts/configs/dhcpcd.conf.ap")
+    subprocess_cmd("mkdir /home/pi/scripts/configs")
+    subprocess_cmd("sudo cp /etc/dhcpcd.conf.ap /home/pi/scripts/configs/dhcpcd.conf.ap")"
     subprocess_cmd("sudo cp /etc/dhcpcd.conf.client /home/pi/scripts/configs/dhcpcd.conf.client")
     subprocess_cmd("sudo cp /etc/dnsmasq.conf.orig /home/pi/scripts/configs/dnsmasq.conf.orig")
 	subprocess_cmd("sudo cp /etc/dnsmasq.conf.ap /home/pi/scripts/configs/dnsmasq.conf.ap")
